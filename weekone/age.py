cf6bb3330e67c965596e64b9c3a335177497bc92
Kabit Khadka <kabitkhadka1@gmail.com>
2026-07-05 17:50:06 +0545

    Age Calculator

---
+++ weekone/age.py
@@ -0,0 +1,8 @@
+birth_year = int(input("Enter your birth year (BS): "))
+current_year = int(input("Enter current BS year: "))
+
+age = current_year - birth_year
+
+print("Your age is", age, "years old in BS calendar.")
+print(" You are", age, "years young.")
+
